d572c736d9c935c0e471e06a9d520cb329b3d9c1
Leon Anavi <leon.anavi@konsulko.com>
2024-06-25 14:24:24 +0300

    rauc_%.bbappend: Grow /data for Rockchip
    Grow data partition for Rockchip devices. Tested on Rock Pi 4 B.
    
    Signed-off-by: Leon Anavi <leon.anavi@konsulko.com>

---
+++ meta-rauc-rockchip/recipes-core/rauc/files/grow-data-partition.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+total_disk_size=$(cat /sys/block/mmcblk1/size)
+
+data_part_size=$(cat /sys/block/mmcblk1/mmcblk1p12/size)
+data_part_start=$(cat /sys/block/mmcblk1/mmcblk1p12/start)
+
+if [ -z "${total_disk_size}" ] || [ -z "${data_part_size}" ] || [ -z "${data_part_start}" ]; then
+    echo "Failed to read disk sizes. Aborting..."
+    exit 1
+fi
+
+data_part_end=$(expr ${data_part_start} + ${data_part_size})
+
+# expr returns 1 (error) if the result is 0, which terminates the script
+# because of 'set -e'. Silence this error
+free_space=$(expr ${total_disk_size} - ${data_part_end} || true)
+
+# If there is less than 8196 blocks = 4 MiB free unused space, we consider
+# the disk as already resized. After resizing, some disk space may still
+# have been left unused.
+if [ ${free_space} -lt 8196 ]; then
+    echo "Disk has already been resized."
+    exit 0
+fi
+
+/usr/bin/umount /dev/mmcblk1p12
+/usr/sbin/sgdisk /dev/mmcblk1 -e
+/usr/sbin/partprobe
+/usr/sbin/parted --script /dev/mmcblk1 resizepart 12 100%
+/usr/sbin/resize2fs /dev/mmcblk1p12
+/usr/bin/umount /dev/mmcblk1p12
+/usr/sbin/e2fsck -fp /dev/mmcblk1p12
+/usr/bin/mount /dev/mmcblk1p12 /data/
+
+echo "Disk resized."
+exit 0
